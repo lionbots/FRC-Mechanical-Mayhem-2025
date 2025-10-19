148d5e2dd9495606b81c607f40b51f322167ecc6
q29ll <lin11239234@gmail.com>
2025-10-18 20:40:42 -0700

    Merge intake branch into test branch

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -9,10 +9,12 @@ import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.IntakeCommand;
 import frc.robot.commands.OuttakeCommand;
 import frc.robot.subsystems.OuttakeSubsystem;
 import frc.robot.commands.ArcadeDriveCommand;
 import frc.robot.subsystems.DrivebaseSubsystem;
+import frc.robot.subsystems.IntakeSubsystem;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -23,6 +25,7 @@ import frc.robot.subsystems.DrivebaseSubsystem;
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
   private final OuttakeSubsystem outtake = new OuttakeSubsystem();
+  private final IntakeSubsystem intake = new IntakeSubsystem();
   private final DrivebaseSubsystem driveBase = new DrivebaseSubsystem();
 
   // Replace with CommandPS4Controller or CommandJoystick if needed
@@ -42,6 +45,7 @@ public class RobotContainer {
   
   private void configureBindings() {
     operatorController.rightTrigger(0.1).whileTrue(new OuttakeCommand(outtake, operatorController::getRightTriggerAxis));
+    operatorController.leftTrigger(0.1).whileTrue((new IntakeCommand(intake, operatorController::getLeftTriggerAxis)));
   }
 
   // Sets up the autonomous mode movements for 3 seconds.
---
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -0,0 +1,47 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IntakeConstants;
+
+//Sparmax library for the motor
+import com.revrobotics.spark.*;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
+public class IntakeSubsystem extends SubsystemBase {
+  private final SparkMax intakeMotor = new SparkMax(IntakeConstants.intakeMotorPort, MotorType.kBrushless);
+  
+  public IntakeSubsystem() {
+    setMotorIdleMode();
+  }
+
+  //Method to set motors to brake mode
+  public void setMotorIdleMode() {
+    SparkMaxConfig idleMode = new SparkMaxConfig();
+    idleMode.idleMode(IdleMode.kBrake);
+
+
+    intakeMotor.configure(idleMode, com.revrobotics.spark.SparkBase.ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+  }
+
+  public void setIntakeSpeed(double intakeSpeed) {
+    intakeMotor.set(intakeSpeed);
+  }
+
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  @Override
+  public void simulationPeriodic() {
+    // This method will be called once per scheduler run during simulation
+  }
+}
