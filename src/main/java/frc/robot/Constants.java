f28819df6d1eea7274791796e89f415cb8b390b6
BruceC-cmd <bruce.christianus@gmail.com>
2025-10-22 19:29:41 -0700

    Spotless fix + syncing with the drive station file

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -22,9 +22,13 @@ public final class Constants {
     public static final int intakeMotorPort = 7;
   }
 
+  public static class OuttakeConstants {
+    public static final int outtakeMotorPort = 7;
+  }
+
   public static class DriveConstants {
     // Constants for motor ports to clarify which one the RoboRio should send electricity too.
-    public static final int leftfrontWheel = 2;
-    public static final int rightfrontWheel = 10;
+    public static final int leftfrontWheel = 7;
+    public static final int rightfrontWheel = 2;
   }
 }
