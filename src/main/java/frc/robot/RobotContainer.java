3b28500f014fb35153b64e7d043649f853d44c84
Adelyn Tran <Tranadelyn5@gmail.com>
2025-11-01 13:53:12 -0700

    spotless fixed:

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,13 +4,10 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.subsystems.IntakeSubsystem;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -30,9 +27,10 @@ public class RobotContainer {
   public RobotContainer() {
     configureBindings();
   }
-  
+
   private void configureBindings() {
-    operatorController.leftTrigger(0.1).whileTrue((new IntakeCommand(intake, operatorController::getLeftTriggerAxis)));
-    
+    operatorController
+        .leftTrigger(0.1)
+        .whileTrue((new IntakeCommand(intake, operatorController::getLeftTriggerAxis)));
   }
 }
